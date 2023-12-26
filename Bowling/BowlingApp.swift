dd64ab60a4e08444ff844034b352fbd6af781307
Luke <loveriverain@gmail.com>
2023-12-26 17:33:27 +0800

    Initial Commit

---
+++ Bowling/BowlingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BowlingApp.swift
+//  Bowling
+//
+//  Created by user on 2023/12/26.
+//
+
+import SwiftUI
+
+@main
+struct BowlingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
